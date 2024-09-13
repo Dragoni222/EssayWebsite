24cccdd5b612db8a72d1a243d994a1d4cc221628
Dragoni222 <joelharr222@gmail.com>
2024-09-12 20:11:03 -0500

    fix error

--- obj/Debug/net8.0/EssayWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/EssayWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EssayWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f27237848c6dbac57dbe223924c60dd848589e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+760db4d6c1a9f7a92f9b0b5488d65279687396b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EssayWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EssayWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
